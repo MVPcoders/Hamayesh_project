b94b6dfcee7dc733845104807b5933c9264ccfc8
capitanmehran <mehrans8192@gmail.com>
2025-05-26 21:50:59 +0330

    nothin special

--- hamayeshProject/urls.py
+++ hamayeshProject/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("index_module.urls"))
+    path('',include("index_module.urls")),
+    path('hamayesh/',include('hamayesh_module.urls'))
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- hamayesh_module/views.py
+++ hamayesh_module/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from hamayesh_module.models import Hamayesh_topics, Hamayesh_prices
 
-# Create your views here.
+
+def HamayeshModule(request):
+    hamayesh_topics = Hamayesh_topics.objects.all()
+    hamayesh_prices = Hamayesh_prices.objects.all()
+    context = {
+        'hamayesh_topics': hamayesh_topics,
+        'hamayesh_prices': hamayesh_prices,
+    }
+    return render(request, 'hamayesh_module/hamayesh_page.html', context)
