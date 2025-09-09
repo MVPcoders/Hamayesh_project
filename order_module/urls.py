416a551499c745b47209209d5e8a2e5af24493fb
capitanmehran <mehrans8192@gmail.com>
2025-09-09 11:34:39 +0330

    درگاه پرداخت همایش میره تو بهاراندیشه فقط الان باید روی سایت تست بشه

--- order_module/urls.py
+++ order_module/urls.py
@@ -1,11 +1,18 @@
+# from django.urls import path
+# from . import views
+#
+# urlpatterns = [
+#     path('', views.pay, name='payment'),
+#     # path("zibal/request/<int:article_id>/", views.send_request_view, name="request"),
+#     # path("zibal/verify/", views.verify_view, name="verify"),
+#     path('request-payment/<int:article_id>', views.send_request, name='request'),
+#     path('verify/', views.verify, name='verify')
+# ]
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.pay, name='payment'),
-    path("zibal/request/<int:article_id>/", views.send_request_view, name="request"),
-    path("zibal/verify/", views.verify_view, name="verify"),
-    path('callback-paytest/', views.callback_gateway_view, name='call'),
-    path('request-payment/<int:article_id>', views.send_request, name='request'),
-    path('verify/', views.verify, name='verify')
+    path("request-payment/<int:article_id>/", views.send_request, name="request"),
+    path("confirm/", views.confirm_payment, name="confirm_payment"),
 ]
--- order_module/views.py
+++ order_module/views.py
@@ -20,31 +20,21 @@ from order_module.utils import apply_discount
 from django.contrib import messages
 import logging
 from django.urls import reverse
-from azbankgateways import (
-    bankfactories,
-    models as bank_models,
-    default_settings as settings,
-)
-from azbankgateways.exceptions import AZBankGatewaysException
 from .zibal_service import zibal_send_request, ZIBAL_STARTPAY
 from .zibal_service import verify_payment
 
 def pay(request):
     return render(request, "order_module/pay.html", context={})
 
-
-
-
-
 merchant_id = "3a681e90-59c0-4511-8101-655b26314ae5"
-ZP_API_REQUEST = "https://sandbox.zarinpal.com/pg/v4/payment/request.json"
-ZP_API_VERIFY = "https://sandbox.zarinpal.com/pg/v4/payment/verify.json"
-ZP_API_STARTPAY = "https://sandbox.zarinpal.com/pg/StartPay/{authority}"
+ZP_API_REQUEST = "https://payment.zarinpal.com/pg/v4/payment/request.json"
+ZP_API_VERIFY = "https://payment.zarinpal.com/pg/v4/payment/verify.json"
+ZP_API_STARTPAY = "https://payment.zarinpal.com/pg/StartPay/{authority}"
 callback_url = "self.callbackURL"
 description = "نهایی کردن خرید شما از سایت ما"
 mobile = "mobile"  # اختیاری
 email = "email"  # اختیاری
-CallbackURL = 'http://localhost:8000/pay/verify/'
+# CallbackURL = 'http://localhost:8000/pay/verify/'
 
 def apply_discount_view(request,article_id):
     if request.method == "POST":
@@ -67,204 +57,228 @@ def apply_discount_view(request,article_id):
     messages.error(request, "خطایی رخ داده است!")
     return redirect('profile')
 
+
+
+
+
+
+# views.py
+
+import json, requests, logging
+from datetime import datetime
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+
+logger = logging.getLogger(__name__)
+
 @login_required
 def send_request(request: HttpRequest, article_id):
-    article_payment, created = Article.objects.get_or_create(is_paid=False, user=request.user.id, id=article_id)
+    try:
+        article_payment = Article.objects.get(id=article_id, user=request.user, is_paid=False)
+    except Article.DoesNotExist:
+        return HttpResponse("سفارش یافت نشد", status=404)
+
     price = int(article_payment.price)
     req_data = {
         "merchant_id": merchant_id,
         "amount": price,
-        "callback_url": CallbackURL,
+        # callback به سایت B
+        "callback_url": "https://baharandishemoalem.ir/order/irn-verify/",
         "description": description,
-        # metadata: {"mobile": request.user.mobile,"email":request.user.email},
     }
     req_headers = {"accept": "application/json", "content-type": "application/json"}
-    req = requests.post(url=ZP_API_REQUEST, data=json.dumps(req_data), headers=req_headers)
-    authority = req.json()['data']['authority']
-    article_payment.send_to_pay = authority
-    article_payment.save()
-    if len(req.json()['errors']) == 0:
+
+    try:
+        req = requests.post(ZP_API_REQUEST, data=json.dumps(req_data), headers=req_headers, timeout=10)
+        response_data = req.json()
+        logger.error(f"Request Response: {response_data}")
+    except Exception:
+        return HttpResponse("خطا در اتصال به درگاه", status=500)
+
+    if "data" in response_data and "authority" in response_data["data"]:
+        authority = response_data["data"]["authority"]
+        article_payment.send_to_pay = authority
+        article_payment.save()
         return redirect(ZP_API_STARTPAY.format(authority=authority))
     else:
-        errors = req.json()['errors']['errors']
-        e_message = req.json()['errors']['message']
-        return HttpResponse(f"error code : {errors}, error message: {e_message}")
-
-
-@login_required
-def verify(request: HttpRequest):
-    t_authority = request.GET['Authority']
-    article_payment = Article.objects.get(send_to_pay=t_authority, user=request.user.id)
-    price = int(article_payment.price)
-    if request.GET.get('Status') == 'OK':
-        req_headers = {"accept": "application/json", "content-type": "application/json"}
-        req_data = {"merchant_id": merchant_id,
-                    "amount": price,
-                    "authority": t_authority, }
-        req = requests.post(url=ZP_API_VERIFY, data=json.dumps(req_data), headers=req_headers)
-        if len(req.json()['errors']) == 0:
-            t_status = req.json()['data']['code']
-            req_id = req.json()["data"]["ref_id"]
-            if t_status == 100:
-
-                article_payment.is_paid = True
-                article_payment.save()
-                amount = price
-                user = request.user if request.user.is_authenticated else None
-                payment = Payment.objects.create(
-                    user=user,
-                    amount=amount,
-                    is_successful=True,
-                    transaction_id=req_id
-                )
-                return render(request, 'order_module/pay.html', {
-                    'success': f"{req_id}",
-                    'amount': price,
-                    'date': datetime.now(),
-                })
-            elif t_status == 101:
-                return render(request, 'order_module/pay.html', {
-                    'info': f"این تراکنش قبلا انجام شده ",
-                })
-            else:
-                return redirect('index')
-        else:
-            e_code = req.json()['errors']['code']
-            e_message = req.json()['errors']['message']
-            return render(request, 'order_module/pay.html', {
-                'info': f"{e_message}",
-            })
-    else:
-        return render(request, 'order_module/pay.html', )
-
-
-def go_to_gateway_view(request):
-    # خواندن مبلغ از هر جایی که مد نظر است
-    amount = 1000000
-    # تنظیم شماره موبایل کاربر از هر جایی که مد نظر است
-    user_mobile_number = "+989112221234"  # اختیاری
-
-    factory = bankfactories.BankFactory()
-    try:
-        bank = (
-            factory.auto_create()
-        )  # or factory.create(bank_models.BankType.BMI) or set identifier
-        bank.set_request(request)
-        bank.set_amount(amount)
-
-        # در صورت تمایل می توانید داده های دلخواه خود را به درگاه ارسال کنید
-        bank.set_custom_data({"foo": "bar"})
-
-
-        # یو آر ال بازگشت به نرم افزار برای ادامه فرآیند
-        bank.set_client_callback_url(reverse("call"))
-        bank.set_mobile_number(user_mobile_number)  # اختیاری
-
-        # در صورت تمایل اتصال این رکورد به رکورد فاکتور یا هر چیزی که بعدا بتوانید ارتباط بین محصول یا خدمات را با این
-        # پرداخت برقرار کنید.
-        bank_record = bank.ready()
-
-        # هدایت کاربر به درگاه بانک
-        return bank.redirect_gateway()
-    except AZBankGatewaysException as e:
-        logging.critical(e)
-        # TODO: redirect to failed page.
-        raise e
-
-
-def callback_gateway_view(request):
-    tracking_code = request.GET.get(settings.TRACKING_CODE_QUERY_PARAM, None)
-    if not tracking_code:
-        logging.debug("این لینک معتبر نیست.")
-        raise Http404
-
-    try:
-        bank_record = bank_models.Bank.objects.get(tracking_code=tracking_code)
-    except bank_models.Bank.DoesNotExist:
-        logging.debug("این لینک معتبر نیست.")
-        raise Http404
-
-    # در این قسمت باید از طریق داده هایی که در بانک رکورد وجود دارد، رکورد متناظر یا هر اقدام مقتضی دیگر را انجام دهیم
-    if bank_record.is_success:
-        # پرداخت با موفقیت انجام پذیرفته است و بانک تایید کرده است.
-        # می توانید کاربر را به صفحه نتیجه هدایت کنید یا نتیجه را نمایش دهید.
-        return HttpResponse("پرداخت با موفقیت انجام شد.")
-
-    # پرداخت موفق نبوده است. اگر پول کم شده است ظرف مدت ۴۸ ساعت پول به حساب شما بازخواهد گشت.
-    return HttpResponse(
-        "پرداخت با شکست مواجه شده است. اگر پول کم شده است ظرف مدت ۴۸ ساعت پول به حساب شما بازخواهد گشت."
-    )
-
-
-
-
-
-
-CALLBACK_URL = "http://localhost:8000/pay/zibal/verify/"  # آدرس برگشت
-
-@login_required
-def send_request_view(request: HttpRequest, article_id):
-    article = get_object_or_404(Article, id=article_id, user=request.user, is_paid=False)
-    amount = int(article.price)
-
-    success, result = zibal_send_request(
-        amount=amount,
-        callback_url=CALLBACK_URL,
-        description=f"پرداخت هزینه مقاله {article.id}",
-        order_id=article.id,
-        mobile=getattr(request.user, "mobile", None),
-    )
-
-    if success:
-        article.send_to_pay = str(result)  # ذخیره trackId
-        article.save()
-        return redirect(ZIBAL_STARTPAY.format(trackId=result))
-    else:
-        messages.error(request, f"خطا در اتصال به درگاه: {result}")
-        return redirect("profile")
+        return HttpResponse("خطا در ایجاد تراکنش")
 
 
 
 @login_required
-def verify_view(request: HttpRequest):
-    track_id = request.GET.get("trackId")
-    if not track_id:
-        return HttpResponse("پارامتر trackId وجود ندارد!")
+def confirm_payment(request: HttpRequest):
+    authority = request.GET.get("authority")
+    ref_id = request.GET.get("ref_id")
+    amount = request.GET.get("amount")
+
+    if not authority or not ref_id:
+        return HttpResponse("اطلاعات ناقص دریافت شد ❌", status=400)
 
     try:
-        article = Article.objects.get(send_to_pay=track_id, user=request.user)
+        article_payment = Article.objects.get(send_to_pay=authority, user=request.user)
     except Article.DoesNotExist:
-        return HttpResponse("مقاله پیدا نشد!")
+        return HttpResponse("سفارش پیدا نشد ❌", status=404)
 
-    amount = int(article.price)
+    article_payment.is_paid = True
+    article_payment.save()
 
-    status, result = verify_payment(track_id=track_id, amount=amount)
+    Payment.objects.create(
+        user=request.user,
+        amount=amount,
+        is_successful=True,
+        transaction_id=ref_id,
+    )
 
-    if status is True:
-        # پرداخت موفق
-        article.is_paid = True
-        article.save()
+    return render(request, "order_module/pay.html", {
+        "success": ref_id,
+        "amount": amount,
+        "date": datetime.now(),
+    })
 
-        payment = Payment.objects.create(
-            user=request.user,
-            amount=amount,
-            is_successful=True,
-            transaction_id=result,  # refNumber
-        )
 
-        return render(request, "order_module/pay.html", {
-            "success": f"پرداخت با موفقیت انجام شد. شماره پیگیری: {result}",
-            "amount": amount,
-            "date": datetime.now(),
-        })
 
-    elif status == "duplicate":
-        return render(request, "order_module/pay.html", {
-            "info": f"این تراکنش قبلاً تایید شده است. شماره پیگیری: {result}",
-        })
 
-    else:
-        return render(request, "order_module/pay.html", {
-            "info": f"پرداخت ناموفق: {result}",
-        })
 
+
+#
+# @login_required
+# def send_request(request: HttpRequest, article_id):
+#     article_payment, created = Article.objects.get_or_create(is_paid=False, user=request.user.id, id=article_id)
+#     price = int(article_payment.price)
+#     req_data = {
+#         "merchant_id": merchant_id,
+#         "amount": price,
+#         "callback_url": CallbackURL,
+#         "description": description,
+#         # metadata: {"mobile": request.user.mobile,"email":request.user.email},
+#     }
+#     req_headers = {"accept": "application/json", "content-type": "application/json"}
+#     req = requests.post(url=ZP_API_REQUEST, data=json.dumps(req_data), headers=req_headers)
+#     authority = req.json()['data']['authority']
+#     article_payment.send_to_pay = authority
+#     article_payment.save()
+#     if len(req.json()['errors']) == 0:
+#         return redirect(ZP_API_STARTPAY.format(authority=authority))
+#     else:
+#         errors = req.json()['errors']['errors']
+#         e_message = req.json()['errors']['message']
+#         return HttpResponse(f"error code : {errors}, error message: {e_message}")
+#
+#
+# @login_required
+# def verify(request: HttpRequest):
+#     t_authority = request.GET['Authority']
+#     article_payment = Article.objects.get(send_to_pay=t_authority, user=request.user.id)
+#     price = int(article_payment.price)
+#     if request.GET.get('Status') == 'OK':
+#         req_headers = {"accept": "application/json", "content-type": "application/json"}
+#         req_data = {"merchant_id": merchant_id,
+#                     "amount": price,
+#                     "authority": t_authority, }
+#         req = requests.post(url=ZP_API_VERIFY, data=json.dumps(req_data), headers=req_headers)
+#         if len(req.json()['errors']) == 0:
+#             t_status = req.json()['data']['code']
+#             req_id = req.json()["data"]["ref_id"]
+#             if t_status == 100:
+#
+#                 article_payment.is_paid = True
+#                 article_payment.save()
+#                 amount = price
+#                 user = request.user if request.user.is_authenticated else None
+#                 payment = Payment.objects.create(
+#                     user=user,
+#                     amount=amount,
+#                     is_successful=True,
+#                     transaction_id=req_id
+#                 )
+#                 return render(request, 'order_module/pay.html', {
+#                     'success': f"{req_id}",
+#                     'amount': price,
+#                     'date': datetime.now(),
+#                 })
+#             elif t_status == 101:
+#                 return render(request, 'order_module/pay.html', {
+#                     'info': f"این تراکنش قبلا انجام شده ",
+#                 })
+#             else:
+#                 return redirect('index')
+#         else:
+#             e_code = req.json()['errors']['code']
+#             e_message = req.json()['errors']['message']
+#             return render(request, 'order_module/pay.html', {
+#                 'info': f"{e_message}",
+#             })
+#     else:
+#         return render(request, 'order_module/pay.html', )
+#
+#
+#
+# CALLBACK_URL = "http://localhost:8000/pay/zibal/verify/"  # آدرس برگشت
+#
+# @login_required
+# def send_request_view(request: HttpRequest, article_id):
+#     article = get_object_or_404(Article, id=article_id, user=request.user, is_paid=False)
+#     amount = int(article.price)
+#
+#     success, result = zibal_send_request(
+#         amount=amount,
+#         callback_url=CALLBACK_URL,
+#         description=f"پرداخت هزینه مقاله {article.id}",
+#         order_id=article.id,
+#         mobile=getattr(request.user, "mobile", None),
+#     )
+#
+#     if success:
+#         article.send_to_pay = str(result)  # ذخیره trackId
+#         article.save()
+#         return redirect(ZIBAL_STARTPAY.format(trackId=result))
+#     else:
+#         messages.error(request, f"خطا در اتصال به درگاه: {result}")
+#         return redirect("profile")
+#
+#
+#
+# @login_required
+# def verify_view(request: HttpRequest):
+#     track_id = request.GET.get("trackId")
+#     if not track_id:
+#         return HttpResponse("پارامتر trackId وجود ندارد!")
+#
+#     try:
+#         article = Article.objects.get(send_to_pay=track_id, user=request.user)
+#     except Article.DoesNotExist:
+#         return HttpResponse("مقاله پیدا نشد!")
+#
+#     amount = int(article.price)
+#
+#     status, result = verify_payment(track_id=track_id, amount=amount)
+#
+#     if status is True:
+#         # پرداخت موفق
+#         article.is_paid = True
+#         article.save()
+#
+#         payment = Payment.objects.create(
+#             user=request.user,
+#             amount=amount,
+#             is_successful=True,
+#             transaction_id=result,  # refNumber
+#         )
+#
+#         return render(request, "order_module/pay.html", {
+#             "success": f"پرداخت با موفقیت انجام شد. شماره پیگیری: {result}",
+#             "amount": amount,
+#             "date": datetime.now(),
+#         })
+#
+#     elif status == "duplicate":
+#         return render(request, "order_module/pay.html", {
+#             "info": f"این تراکنش قبلاً تایید شده است. شماره پیگیری: {result}",
+#         })
+#
+#     else:
+#         return render(request, "order_module/pay.html", {
+#             "info": f"پرداخت ناموفق: {result}",
+#         })
+#
